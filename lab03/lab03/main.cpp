bff2f6ca0edd521800cef39869feb49dc9887c7c
russ <russ@DESKTOP>
2016-10-15 12:08:31 -0700

    implemented function to match the number of bits in the two binary number strings

--- lab03/lab03/main.cpp
+++ lab03/lab03/main.cpp
@@ -11,13 +11,19 @@ http://www.cplusplus.com/forum/general/103479/
 using namespace std;
 
 string checkInput();
+void matchSizes(string &bin1, string &bin2);
 
 int main() {
   cout << "Enter First Binary Number: ";
-  string binaryString1 = checkInput();
+  string bin1 = checkInput();
 
   cout << "Enter Second Binary Number: ";
-  string binaryString2 = checkInput();
+  string bin2 = checkInput();
+
+  matchSizes(bin1, bin2);
+
+  cout << bin1 << " - " << bin1.size() << endl;
+  cout << bin2 << " - " << bin2.size() << endl;
 
   return 0;
 }
@@ -33,11 +39,26 @@ string checkInput() {
       if ((tempStr.compare(i, 1, "0") != 0) &&
           (tempStr.compare(i, 1, "1") != 0)) {
         valid = false;
-        std::cout << "Enter Valid Binary Number: ";
+        cout << "Enter Valid Binary Number: ";
         break;
       }
     }
   } while (valid == false);
 
   return tempStr;
+}
+
+// left-fills the shorter binary number with 0's if str1 & str2 have different
+// sizes
+void matchSizes(string &bin1, string &bin2) {
+  if (bin1.size() != bin2.size()) {
+    string big, small;
+    if (bin1.size() > bin2.size()) {
+      size_t dif = bin1.size() - bin2.size();
+      bin2.insert(0, dif, '0');
+    } else {
+      size_t dif = bin2.size() - bin1.size();
+      bin1.insert(0, dif, '0');
+    }
+  }
 }
