8eae83fed6c21b6ae770109df263b779c6bbf588
russ <russ@DESKTOP>
2016-11-05 11:47:34 -0700

    created lab04, basically finished it as well

---
+++ lab04/lab04/main.cpp
@@ -0,0 +1,71 @@
+/*
+NAME......: Russ Gould
+CRN.......: 41726
+ASSIGNMENT: Lab4
+SOURCES...: none
+*/
+
+#include <iomanip>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPrime(int num);
+void getFactors(int num, vector<int> &factors);
+
+int main() {
+  for (int i(0); i < 4; i++) {
+    cout << "Please enter an integer that you would like the prime factors of:";
+    int num(0);
+    cin >> num;
+    vector<int> factors;
+    if (isPrime(num)) {
+      cout << "That number is prime." << endl;
+      cout << endl;
+    } else {
+      getFactors(num, factors);
+      cout << "The prime factors of " << num << " are: ";
+      for (int t(0); t < factors.size(); t++) {
+        if (t == factors.size() - 1) {
+          cout << factors[t] << "." << endl;
+        } else {
+          cout << factors[t] << ", ";
+        }
+      }
+      cout << endl;
+    }
+  }
+  return 0;
+}
+
+bool isPrime(int num) {
+  if (num <= 1) {
+    return false;
+  } else if (num == 2) {
+    return true;
+  } else if (num % 2 == 0) {
+    return false;
+  } else {
+    for (int t(3); t * t <= num; t++) {
+      if (num % t == 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+void getFactors(int num, vector<int> &factors) {
+  if (isPrime(num)) { // only final factor will satisfy this
+    factors.push_back(num);
+    return;
+  } else {
+    for (int t(2); t <= (num / 2); t++) {
+      if (isPrime(t) && num % t == 0) {
+        factors.push_back(t);
+        getFactors(num / t, factors);
+        break;
+      }
+    }
+  }
+}
