da9d26ca761bb216852b5ab54b6403af7e36d3c7
russ <russ@DESKTOP>
2017-01-24 14:45:22 -0800

    mostly completed

--- lab07/lab07/main.cpp
+++ lab07/lab07/main.cpp
@@ -5,60 +5,59 @@ ASSIGNMENT: Lab7
 SOURCES...: none
 */
 
-#include <cmath>
 #include <iostream>
 using namespace std;
 
 void getRoots(float A, float B, float &t1, float &t2, float disc);
 void getSolution1(float &C, float &D, float a0, float a1, float t1);
 void getSolution2(float &C, float &D, float a0, float a1, float t1, float t2);
-// void outputSequence();
 
 int main() {
-  float A(0), B(0), C(0), D(0), a0(0), a1(0), t1(1), t2(1), disc(0);
-  cout << "Please enter the following values:" << endl;
-  cout << "A=";
-  cin >> A;
-  cout << "B=";
-  cin >> B;
-  cout << "a0=";
-  cin >> a0;
-  cout << "a1=";
-  cin >> a1;
+  for (int i(0); i < 4; i++) {
+    float A(0), B(0), C(0), D(0), a0(0), a1(0), t1(1), t2(1), disc(0);
+    cout << "Please enter the following values:" << endl;
+    cout << "A=";
+    cin >> A;
+    cout << "B=";
+    cin >> B;
+    cout << "a0=";
+    cin >> a0;
+    cout << "a1=";
+    cin >> a1;
 
-  disc = (A * A) + (4 * B);
+    disc = (A * A) + (4 * B);
 
-  if (disc < 0) {
-    cout << "Complex roots, run away!" << endl;
-  } else {
-    if (disc == 0) {
-      t1 = A / 2;
-      getSolution1(C, D, a0, a1, t1);
-      cout << endl;
-      cout << "The formula would be:" << endl;
-      cout << "a_n = (" << C << ")(" << t1 << ")^n + (" << D << ")n(" << t1
-           << ")^n" << endl;
+    if (disc < 0) {
+      cout << "Complex roots, run away!" << endl;
     } else {
-      getRoots(A, B, t1, t2, disc);
-      getSolution2(C, D, a0, a1, t1, t2);
-      cout << endl;
-      cout << "The formula would be:" << endl;
-      cout << "a_n = (" << C << ")(" << t1 << ")^n + (" << D << ")(" << t2
-           << ")^n" << endl;
-    }
+      if (disc == 0) {
+        t1 = A / 2;
+        getSolution1(C, D, a0, a1, t1);
+        cout << endl;
+        cout << "The formula would be:" << endl;
+        cout << "a_n = (" << C << ")(" << t1 << ")^n + (" << D << ")n(" << t1
+             << ")^n" << endl;
+      } else {
+        getRoots(A, B, t1, t2, disc);
+        getSolution2(C, D, a0, a1, t1, t2);
+        cout << endl;
+        cout << "The formula would be:" << endl;
+        cout << "a_n = (" << C << ")(" << t1 << ")^n + (" << D << ")(" << t2
+             << ")^n" << endl;
+      }
 
-    float a2(a0), a3(a1), a4(0);
-    cout << endl;
-    cout << "a0= " << a2 << endl;
-    cout << "a1= " << a3 << endl;
-    for (int i(2); i < 6; i++) {
-      a4 = (A * a3) + (B * a2);
-      cout << "a" << i << "= " << a4 << endl;
-      a2 = a3;
-      a3 = a4;
+      float a2(a0), a3(a1), a4(0);
+      cout << endl;
+      cout << "a0= " << a2 << endl;
+      cout << "a1= " << a3 << endl;
+      for (int i(2); i < 6; i++) {
+        a4 = (A * a3) + (B * a2);
+        cout << "a" << i << "= " << a4 << endl;
+        a2 = a3;
+        a3 = a4;
+      }
     }
   }
-
   return 0;
 }
 
---
+++ lab25101/lab25101/main.cpp
@@ -0,0 +1,192 @@
+/*
+NAME......: Russ Gould
+CRN.......: 11674
+ASSIGNMENT: Lab01
+SOURCES...: none
+*/
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+void outputPair(vector<pair<string, string>> vect);
+void output(vector<string> vect);
+void setCard(vector<string> vect);
+void setRoster(vector<string> vect);
+vector<string> setUnion(vector<string> setA, vector<string> setB);
+vector<string> setIntersect(vector<string> setA, vector<string> setB);
+vector<string> setComplement(vector<string> setA, vector<string> setB);
+vector<pair<string, string>> setCross(vector<string> setA, vector<string> setB);
+
+int main() {
+  cout << "Enter set A:" << endl;
+  string input;
+  string buf;
+  getline(cin, input);
+  stringstream ssin(input);
+  vector<string> setA;
+
+  while (ssin >> buf) {
+    setA.push_back(buf);
+  }
+
+  cout << "Enter set B:" << endl;
+  string input2;
+  string buf2;
+  getline(cin, input2);
+  stringstream ssin2(input2);
+  vector<string> setB;
+
+  while (ssin2 >> buf2) {
+    setB.push_back(buf2);
+  }
+
+  vector<string> setU = setUnion(setA, setB);
+  cout << endl;
+  cout << "Union:" << endl;
+  output(setU);
+
+  vector<string> setI = setIntersect(setA, setB);
+  cout << endl;
+  cout << "Intersection:" << endl;
+  output(setI);
+
+  vector<string> setC1 = setComplement(setA, setB);
+  cout << endl;
+  cout << "A - B:" << endl;
+  output(setC1);
+
+  vector<string> setC2 = setComplement(setB, setA);
+  cout << endl;
+  cout << "B - A:" << endl;
+  output(setC2);
+
+  vector<pair<string, string>> setProd = setCross(setA, setB);
+  cout << endl;
+  cout << "Cross Product:" << endl;
+  outputPair(setProd);
+
+  cout << endl;
+  cout << "Cardinality of Power Set of Cross Product:" << pow(2, setProd.size())
+       << endl;
+
+  return 0;
+}
+
+void outputPair(vector<pair<string, string>> vect) {
+  cout << "Cardinality: ";
+  cout << vect.size() << endl;
+  cout << "Roster: ";
+  for (size_t i = 0; i < vect.size(); i++) {
+    if (vect.size() == 0) {
+      cout << "{}";
+    } else if (vect.size() == 1) {
+      cout << "{"
+           << "(" << vect[i].first << "," << vect[i].second << ")"
+           << "}";
+    } else if (i == 0) {
+      cout << "{"
+           << "(" << vect[i].first << "," << vect[i].second << ")";
+    } else if (i == vect.size() - 1) {
+      cout << ", "
+           << "(" << vect[i].first << "," << vect[i].second << ")"
+           << "}";
+    } else {
+      cout << ", "
+           << "(" << vect[i].first << "," << vect[i].second << ")";
+    }
+  }
+  cout << endl;
+}
+
+void output(vector<string> vect) {
+  cout << "Cardinality: ";
+  setCard(vect);
+  cout << "Roster: ";
+  setRoster(vect);
+}
+
+void setCard(vector<string> vect) { cout << vect.size() << endl; }
+
+void setRoster(vector<string> vect) {
+  for (size_t i = 0; i < vect.size(); i++) {
+    if (vect.size() == 0) {
+      cout << "{}";
+    } else if (vect.size() == 1) {
+      cout << "{" << vect[i] << "}";
+    } else if (i == 0) {
+      cout << "{" << vect[i];
+    } else if (i == vect.size() - 1) {
+      cout << ", " << vect[i] << "}";
+    } else {
+      cout << ", " << vect[i];
+    }
+  }
+  cout << endl;
+}
+
+vector<string> setUnion(vector<string> setA, vector<string> setB) {
+  vector<string> result = {};
+  for (size_t i = 0; i < setA.size(); i++) {
+    result.push_back(setA[i]);
+  }
+  for (size_t i = 0; i < setB.size(); i++) {
+    bool flag = false;
+    for (size_t t = 0; t < result.size(); t++) {
+      if (setB[i] == result[t]) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      result.push_back(setB[i]);
+    }
+  }
+  return result;
+}
+
+vector<string> setIntersect(vector<string> setA, vector<string> setB) {
+  vector<string> result = {};
+  for (size_t i = 0; i < setA.size(); i++) {
+    for (size_t t = 0; t < setB.size(); t++) {
+      if (setA[i] == setB[t]) {
+        result.push_back(setA[i]);
+        break;
+      }
+    }
+  }
+  return result;
+}
+
+vector<string> setComplement(vector<string> setA, vector<string> setB) {
+  vector<string> result = {};
+
+  for (size_t i = 0; i < setA.size(); i++) {
+    bool flag = false;
+    for (size_t t = 0; t < setB.size(); t++) {
+      if (setA[i] == setB[t]) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) {
+      result.push_back(setA[i]);
+    }
+  }
+
+  return result;
+}
+
+vector<pair<string, string>> setCross(vector<string> setA,
+                                      vector<string> setB) {
+  vector<pair<string, string>> result = {};
+  for (size_t i = 0; i < setA.size(); i++) {
+    for (size_t t = 0; t < setB.size(); t++) {
+      result.push_back(make_pair(setA[i], setB[t]));
+    }
+  }
+  return result;
+}
